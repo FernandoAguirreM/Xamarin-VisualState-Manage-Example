cb833999b2dc3a7014d7b3a2c6d1903088e136c1
Fernando Montañez <fernando_aguirre.m@hotmail.com>
2021-06-10 22:19:55 -0500

    Add example of how to use the visual state manager

---
+++ Xamarin_VisualState_Example/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_VisualState_Example
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool FlagDisableEntry { get; set; } = true;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisableEntry.IsEnabled = FlagDisableEntry = !FlagDisableEntry;
+        }
+    }
+}
